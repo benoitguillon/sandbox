8d5d375f55d7e875df7d92c6d5a33cb638577914
Benoit Guillon <benoit.guillon@gmail.com>
2013-12-25 23:01:01 +0100

    add groovy projects

---
+++ groovy/groovy1/src/test/java/org/bgi/GroovyTestCase.java
@@ -0,0 +1,15 @@
+package org.bgi;
+
+import groovy.util.GroovyScriptEngine;
+
+import org.junit.Test;
+
+public class GroovyTestCase {
+	
+	@Test
+	public void test1() throws Exception {
+		
+		GroovyScriptEngine gse = new GroovyScriptEngine(rc);
+	}
+
+}
